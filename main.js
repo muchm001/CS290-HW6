0d5f93ac9787c0116b6c2881ae2b1a31e6ecae0b
Michael <muchm001@gmail.com>
2021-03-12 18:11:48 -0600

    ok

--- main.js
+++ main.js
@@ -9,7 +9,17 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-var mysql = require('./dbcon.js');
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit : 10,
+  host : 'classmysql.engr.oregonstate.edu',
+  user : 'cs290_uchmanom',
+  password : '8326',
+  database : 'cs290_uchmanom',
+  dateStrings : 'date'
+});
+
+module.exports.pool = pool;
 
 app.use(express.static('public'));
 

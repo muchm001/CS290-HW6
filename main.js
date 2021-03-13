242aa0018802ba1cbf78c7d1a69c4d2b15f8351f
Michael <muchm001@gmail.com>
2021-03-12 20:52:08 -0600

    ok

--- main.js
+++ main.js
@@ -1,22 +1,43 @@
-var express = require('express');
+var express = require("express");
 
 var app = express();
-var handlebars = require('express-handlebars').create({defaultLayout:'main'});
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
+var handlebars = require("express-handlebars").create({defaultLayout: "main"});
+var request = require('request');
+var bodyParser = require("body-parser");
+var helpers = require('handlebars-helpers')();
 
-var mysql = require('./dbcon.js');
 
-app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(express.static(__dirname + '/public'));
 
-app.engine('handlebars', handlebars.engine);
+app.engine('handlebars',handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 22420);
+app.set('port',22420);
+
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit : 10,
+  host : 'classmysql.engr.oregonstate.edu',
+  user : 'cs290_uchmanom',
+  password : '8326',
+  database : 'cs290_uchmanom',
+  dateStrings : 'date'
+});
+
+var con = mysql.createConnection({
+  host: 'classmysql.engr.oregonstate.edu',
+  user: 'cs290_latimerh',
+  password: 'Chickens-123'
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 app.get('/reset-table',function(req,res,next){
   var context = {};
-  mysql.pool.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
+  pool.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
     var createString = "CREATE TABLE workouts("+
         "id INT PRIMARY KEY AUTO_INCREMENT,"+
         "name VARCHAR(255) NOT NULL,"+
@@ -24,166 +45,102 @@ app.get('/reset-table',function(req,res,next){
         "weight INT,"+
         "date DATE,"+
         "lbs BOOLEAN)";
-    mysql.pool.query(createString, function(err){
+    pool.query(createString, function(err){
       context.results = "Table reset";
       res.render('home',context);
     })
   });
 });
 
-app.get('/', function(req, res, next){
+app.post('/insert',function(req,res,next){
   var context = {};
-
-  //console.log("GET Request Received By Server!");
-
-  if(req.query.generateTable){  // This will handle get request sent from client side after page first rendered
-
-    mysql.pool.query('SELECT id, name, reps, weight, date_format(date,"%m-%d-%Y") AS date, lbs FROM workouts', function(err, rows, fields){
-      if (err){
-        next(err);
-        return;
-      }
-
-      res.send(JSON.stringify(rows));
-
-    });
-
-  }
-
-  else{
-    res.render('home');
-  }
-
-});
-
-app.post('/', function(req,res,next){
-  var context = {};
-  //context.results = "You've sent a POST request";
-
-  //console.log("POST Request Received By Server!");
-  //console.log(req.body);
-
-  if(req.body.addExerciseButton){   // We came in from the add exercise form so need to add a row to the database table
-                                    // if user didn't enter a first name, we don't want to add it to the table
-    if(req.body.name == ""){
-      var sendError = {badInput: true};
-      res.send(JSON.stringify(sendError));
-      return;
-    }
-
-    mysql.pool.query('INSERT INTO workouts (name, reps, weight, date, lbs) VALUES (?,?,?,?,?)', [req.body.name, req.body.reps, req.body.weight, req.body.date, req.body.lbs], function(err, result){
-      if (err) {
-        next(err);
-        return;
-      }
-
-      else {
-        mysql.pool.query('SELECT id, name, reps, weight, date_format(date,"%m-%d-%Y") AS date, lbs FROM workouts', function (err, rows, fields) {
-          if (err) {
+  console.log("body: %j", req.body)
+  if(req.body.name != '' ){
+    pool.query("INSERT INTO workouts (`name`,`reps`,`weight`, `date`, `lbs`) VALUES (?,?,?,?,?)",
+        [req.body.name, req.body.reps, req.body.weight, req.body.date, req.body.lbs], function(err, result){
+          if(err){
             next(err);
             return;
           }
-
-          else {
-            res.send(JSON.stringify(rows));
-          }
+          res.render('home',context);
         });
-      }
-
-    });
+  }else {
+    console.log("is empty");
   }
+});
 
-  else if(req.body.deleteButton){     // We're coming in from the delete button so need to remove a row
-    mysql.pool.query('DELETE FROM workouts WHERE id = ?', [req.body.id], function(err, result) {
-      if (err) {
-        next(err);
-        return;
-      }
-
-      else {
-        mysql.pool.query('SELECT id, name, reps, weight, date_format(date,"%m-%d-%Y") AS date, lbs FROM workouts', function (err, rows, fields) {
-          if (err) {
-            next(err);
-            return;
-          }
-
-          else {
-            res.send(JSON.stringify(rows));
-          }
-
-        });
-      }
-
-    });
-  }
-
-  else if(req.body.updateButton) {      // User entered from the edit button. need to alter current page to show update form
-
-    // Select all info in the row user wants to update
-    mysql.pool.query('SELECT * FROM workouts WHERE id = ?', [req.body.id], function(err, rows, fields) {
-      if (err) {
-        next(err);
-        return;
-      }
-
-      else {
-        context.updateForm = true;
-        context.id = rows[0].id;
-        context.name = rows[0].name;
-        context.reps = rows[0].reps;
-        context.weight = rows[0].weight;
-        context.date = rows[0].date;
-
-        if(rows[0].lbs == 1) {
-          context.lbsUsed = true;
-        }
-
-        res.render('home', context);
-      }
-
-    });
-
-  }
-
-  else if(req.body.updateExerciseButton) {      // user has submitted the update exercise form
-
-    // if user deleted the exercise name, this should not be allowed. Display an error message
-
-    if(req.body.name == ""){
-      context.errorResults = "Update FAILED: All rows must contain at least an exercise name. Try again!";
-      res.render('home',context);
+app.get('/',function(req,res,next){
+  var context = {};
+  pool.query('SELECT * FROM workouts', function(err, rows, fields){
+    if(err){
+      next(err);
       return;
     }
-
-    mysql.pool.query('UPDATE workouts SET name = ?, reps = ?, weight = ?, date = ?, lbs = ? WHERE id = ?', [req.body.name, req.body.reps, req.body.weight, req.body.date, req.body.lbs, req.body.id], function(err, result) {
-      if(err) {
-        next(err);
-        return;
-      }
-
-      else {
-        res.render('home');
-
-      }
-
-    });
-  }
-
-
+    context.results = rows;
+    console.log(context.results);
+    res.render('home', context);
+  });
 });
 
-app.use(function(req,res){
-  res.status(404);
-  res.render('404');
+app.get('/updatedTable',function(req,res,next){
+  var context = {};
+  pool.query('SELECT * FROM workouts', function(err, rows, fields){
+    if(err){
+      next(err);
+      return;
+    }
+    context.results = rows;
+    console.log(context.results);
+    res.send(context);
+  });
 });
 
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.type('plain/text');
-  res.status(500);
-  res.render('500');
+
+app.get('/update',function(req,res,next){
+  var context = {};
+  pool.query("SELECT * FROM workouts WHERE id=?", [req.query.id], function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    context.result = result;
+    res.render('update',context);
+  });
 });
+
+app.get('/safe-update',function(req,res,next){
+  var context = {};
+  pool.query("SELECT * FROM workouts WHERE id=?", [req.query.id], function(err, result){
+    console.log("in the safe update");
+    if(err){
+      next(err);
+      return;
+    }
+    if(result.length == 1){
+      pool.query("UPDATE workouts SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=? ",
+          [req.query.name, req.query.reps, req.query.weight ,req.query.date,req.query.lbs ,req.query.id],
+          function(err, result){
+            if(err){
+              next(err);
+              return;
+            }
+            res.render('home');
+          });
+    }
+  });
+});
+
+app.get('/delete',function(req,res,nest){
+  pool.query("delete from workouts where id=?", [req.query.id], function(err,result){
+    console.log("in the delete function");
+    if(err){
+      next(err);
+      return;
+    }
+  });
+});
+
 
 app.listen(app.get('port'), function(){
-  console.log('Express started on http://flip2.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
+  console.log('Express started on http://flip3.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
--- public/script.js
+++ public/script.js
@@ -1,264 +1,122 @@
-function deleteRow(id){
-    console.log("Row with id# " + id + " will be deleted");
-}
+document.addEventListener('DOMContentLoaded', bindButtons);
 
-function displayTable(newData){
-    if(document.getElementById("dataTable")){
-        document.getElementById("tableToBeConstructed").removeChild(document.getElementById("dataTable"));
-    }
+function bindButtons(){
+    document.getElementById('submit').addEventListener('click', function(event){
 
-    // Construct a new table on the HTML page using returned data from database
+        var req = new XMLHttpRequest();
+        var parms = 'name=';
+        parms += document.getElementById('name').value;
+        parms += '&reps=' + document.getElementById('reps').value;
+        parms += '&weight=' + document.getElementById('weight').value;
+        parms += '&date=' + document.getElementById('date').value;
 
-    var newTable = document.createElement("table");
-    newTable.id = "dataTable";
-    newTable.style.border = "solid";
-    newTable.style.borderWidth = "1px";
-
-    var headRow = document.createElement("tr");
-
-    var nameHeader = document.createElement("th");
-    nameHeader.textContent = "Exercise Name";
-    nameHeader.style.border = "solid";
-    nameHeader.style.borderWidth = "1px";
-    nameHeader.style.textAlign = "center";
-    headRow.appendChild(nameHeader);
-
-    var repsHeader = document.createElement("th");
-    repsHeader.textContent = "Number of Reps";
-    repsHeader.style.border = "solid";
-    repsHeader.style.borderWidth = "1px";
-    repsHeader.style.textAlign = "center";
-    headRow.appendChild(repsHeader);
-
-    var weightHeader = document.createElement("th");
-    weightHeader.textContent = "Weight Used";
-    weightHeader.style.border = "solid";
-    weightHeader.style.borderWidth = "1px";
-    weightHeader.style.textAlign = "center";
-    headRow.appendChild(weightHeader);
-
-    var dateHeader = document.createElement("th");
-    dateHeader.textContent = "Date Performed";
-    dateHeader.style.border = "solid";
-    dateHeader.style.borderWidth = "1px";
-    dateHeader.style.textAlign = "center";
-    headRow.appendChild(dateHeader);
-
-    var lbsHeader = document.createElement("th");
-    lbsHeader.textContent = "Unit";
-    lbsHeader.style.border = "solid";
-    lbsHeader.style.borderWidth = "1px";
-    lbsHeader.style.textAlign = "center";
-    headRow.appendChild(lbsHeader);
-
-
-    newTable.appendChild(headRow);
-
-
-    // Now populate the table with the results returned from the database
-    for(var index = 0; index < newData.length; index++){
-        var newRow = document.createElement("tr");
-
-        var nameCell = document.createElement("td");
-        nameCell.textContent = newData[index].name;
-        nameCell.style.border = "solid";
-        nameCell.style.borderWidth = "1px";
-        nameCell.style.textAlign = "center";
-        newRow.appendChild(nameCell);
-
-        var repsCell = document.createElement("td");
-        repsCell.textContent = newData[index].reps;
-        repsCell.style.border = "solid";
-        repsCell.style.borderWidth = "1px";
-        repsCell.style.textAlign = "center";
-        newRow.appendChild(repsCell);
-
-        var weightCell = document.createElement("td");
-        weightCell.textContent = newData[index].weight;
-        weightCell.style.border = "solid";
-        weightCell.style.borderWidth = "1px";
-        weightCell.style.textAlign = "center";
-        newRow.appendChild(weightCell);
-
-        var dateCell = document.createElement("td");
-        dateCell.textContent = newData[index].date;
-        dateCell.style.border = "solid";
-        dateCell.style.borderWidth = "1px";
-        dateCell.style.textAlign = "center";
-        newRow.appendChild(dateCell);
-
-        var lbsCell = document.createElement("td");
-        if(newData[index].lbs == 1) {
-            lbsCell.textContent = "lbs";
+        var checkBox = document.getElementById('lbs')
+        console.log(checkBox.checked);
+        if(checkBox.checked){
+            parms += "&lbs=1";
         }
-
-        else {
-            lbsCell.textContent = "kg";
+        else{
+            parms += "&lbs=0";
         }
-
-        lbsCell.style.border = "solid";
-        lbsCell.style.borderWidth = "1px";
-        lbsCell.style.textAlign = "center";
-        newRow.appendChild(lbsCell);
-
-        // Store the id for the row in a hidden input
-
-        var deleteContainer = document.createElement("td");
-        var deleteForm = document.createElement("form");
-        deleteForm.method="post";                                   // Need to specify the form's action on next line
-        deleteForm.action="";
-
-        var hiddenDeleteId = document.createElement("input");
-        hiddenDeleteId.type="hidden";
-        hiddenDeleteId.className="hiddenId";                  // Used to determine which row will be deleted
-        hiddenDeleteId.name="id";
-        hiddenDeleteId.value = newData[index].id;   // Store the id within the form
-        deleteForm.appendChild(hiddenDeleteId);
-
-        // Form submits only the id of the row to the given page
-
-        var deleteButton = document.createElement("input");
-        deleteButton.type="submit";
-        deleteButton.name="deleteButton";
-        deleteButton.value="Delete";
-        deleteButton.className="deleteButton";
-        deleteForm.appendChild(deleteButton);
-        deleteContainer.appendChild(deleteForm);
-        newRow.appendChild(deleteContainer);
-
-        var updateContainer = document.createElement("td");     // Code to create an edit button that submits row id to server
-        var updateForm = document.createElement("form");
-        updateForm.method = "post";
-        updateForm.action = "";
-        var hiddenUpdateId = document.createElement("input");
-        hiddenUpdateId.type = "hidden";
-        hiddenUpdateId.className = "updateId";
-        hiddenUpdateId.name = "id";
-        hiddenUpdateId.value = newData[index].id;     // Store the row id within the form
-        updateForm.appendChild(hiddenUpdateId);
-
-        var updateButton = document.createElement("input");
-        updateButton.type = "submit";
-        updateButton.name = "updateButton";
-        updateButton.value = "Edit";
-        updateButton.className = "updateButton";
-        updateForm.appendChild(updateButton);
-        updateContainer.appendChild(updateForm);
-        newRow.appendChild(updateContainer);
+        //parms += '&lbs=' + document.getElementById('lbs').value;
 
 
-        newTable.appendChild(newRow);
-    }
+        //Post newest entry to database
+        req.open('POST', 'http://flip3.engr.oregonstate.edu:62619/insert', true);
+        req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        req.addEventListener('load',function(){
+            if(req.status >= 200 && req.status < 400){
+                // Query database and get newest entry
+                var req2 = new XMLHttpRequest();
+                req2.open("GET", "http://flip3.engr.oregonstate.edu:62619/updatedTable", true);
+                req2.addEventListener('load',function(){
+                    if(req2.status >= 200 && req2.status < 400){
 
-    document.getElementById("tableToBeConstructed").appendChild(newTable);
+                        //Get newest record from database
+                        var response = req2.responseText;
+                        var response = JSON.parse(req2.responseText);
+                        newEntry = response.results[response.results.length-1];
+                        newID = response.results[response.results.length-1].id;
+
+                        // Create a new row and add details of newest record
+                        var newRow = document.createElement("tr");
+                        newRow.id = newID;
+
+                        var newRowDetailname = document.createElement("td");
+                        newRowDetailname.textContent = newEntry.name;
+                        newRow.appendChild(newRowDetailname);
+
+                        var newRowDetailreps = document.createElement("td");
+                        newRowDetailreps.textContent = newEntry.reps;
+                        newRow.appendChild(newRowDetailreps);
+
+                        var newRowDetailweight = document.createElement("td");
+                        newRowDetailweight.textContent = newEntry.weight;
+                        newRow.appendChild(newRowDetailweight);
+
+                        var date = new Date(response.results[response.results.length-1].date);
+                        var newRowDetaildate = document.createElement("td");
+                        newRowDetaildate.textContent = date.getMonth()+1 + "-" + date.getDate() + "-" + date.getFullYear();
+                        newRow.appendChild(newRowDetaildate);
+
+                        var newRowDetaillbs = document.createElement("td");
+                        if (response.results[response.results.length-1].lbs === 0){
+                            newRowDetaillbs.textContent = "kgs";
+                        } else {
+                            newRowDetaillbs.textContent = "lbs";
+                        }
+                        newRow.appendChild(newRowDetaillbs);
+
+                        //Create edit button for new record
+                        var newRowDetailEdit = document.createElement("td");
+                        var editButton = document.createElement("input");
+                        editButton.id = "edit2";
+                        editButton.type = "button";
+                        editButton.value = "Edit";
+                        var connectString = "'/update?id="+ newID + "'"
+                        editButton.setAttribute('onclick', "window.location.href=" + connectString);
+                        newRowDetailEdit.appendChild(editButton);
+                        newRow.appendChild(newRowDetailEdit);
+
+                        //Create delete button for new record
+                        var newRowDetailDelete = document.createElement("td");
+                        var deleteButton = document.createElement("input");
+                        deleteButton.id = "delete";
+                        deleteButton.type = "button";
+                        deleteButton.value = "Delete";
+                        deleteButton.setAttribute('onclick',"deleteRow("+newID+")");
+
+                        newRowDetailDelete.appendChild(deleteButton);
+                        newRow.appendChild(newRowDetailDelete);
+
+                        // Append new row with buttons to existing table
+                        document.getElementById("table").appendChild(newRow);
+
+                        event.preventDefault();
+                    } else {
+                        console.log("Error in network request: " + req2.statusText);
+                    }});
+                req2.send(null);
+                event.preventDefault();
+            } else {
+                console.log("Error in network request: " + req.statusText);
+            }});
+        req.send(parms);
+        event.preventDefault();
+    })
 };
 
-document.addEventListener("DOMContentLoaded",function(){
+// delete function assigned to every delete button
+function deleteRow(id){
     var req = new XMLHttpRequest();
-    req.open("GET", "http://flip2.engr.oregonstate.edu:22420/?generateTable=1", true);
-    req.addEventListener("load", function(){
-        if (req.status >= 200 && req.status < 400) {
-            //console.log("GET Request Worked");
-            var dataTable = JSON.parse(req.responseText);
-            displayTable(dataTable);
-            bindDeleteButtons();
-        }
+    var parms = 'id=';
+    parms += id;
 
-        else {
-            console.log("Get Request Failed");
-        }
-    });
+    var el = document.getElementById(id);
+    el.remove();
 
+    req.open("GET", "http://flip3.engr.oregonstate.edu:62619/delete?"+parms, true);
     req.send(null);
-
-});
-
-
-
-document.getElementById('addExerciseButton').addEventListener("click", function(event){
-    //console.log("POST Event Listener Simple Method Worked!");
-    var req = new XMLHttpRequest();
-    // Get all of the user input information
-
-    var dataToSend = {};
-    dataToSend.name = document.getElementById('name').value;
-    dataToSend.reps = document.getElementById('reps').value;
-    dataToSend.weight = document.getElementById('weight').value;
-    dataToSend.date = document.getElementById('date').value;
-    dataToSend.lbs = document.getElementById('lbs').value;
-    dataToSend.addExerciseButton = document.getElementById('addExerciseButton').value;
-
-
-
-    req.open("POST", "http://flip2.engr.oregonstate.edu:22420/", true);
-    req.setRequestHeader('Content-Type', 'application/json');
-    req.addEventListener("load", function(){
-        if (req.status >= 200 && req.status < 400) {
-            //console.log("POST Request Worked");
-            //console.log(req.responseText);
-
-
-            var newData = JSON.parse(req.responseText);
-
-            // if user forgot to include the name of the exercise an error will occur
-
-            if(newData.badInput){
-                alert("You need to input the name of the exercise to add a row to the table. Try again.");
-            }
-
-            else{
-                displayTable(newData);
-                bindDeleteButtons();
-            }
-
-        }
-
-        else {
-            console.log("POST Request Failed");
-        }
-    });
-
-    req.send(JSON.stringify(dataToSend));
-
     event.preventDefault();
-});
-
-
-function bindDeleteButtons(){
-    //console.log("Bind delete buttons was started");
-    var deleteButtonList = document.getElementsByClassName("deleteButton"); // List of all form submit buttons that will send id to server
-    var hiddenIdList = document.getElementsByClassName("hiddenId");         // List of all hidden inputs that contain id as value
-
-
-    for (var index = 0; index < deleteButtonList.length; index++){
-        function closureWrapper(index) {
-            deleteButtonList[index].addEventListener("click", function(event){
-
-                var req = new XMLHttpRequest();
-                var dataToSend = {};
-                req.open("POST", "http://flip2.engr.oregonstate.edu:22420/", true);
-                req.setRequestHeader('Content-Type', 'application/json');
-
-                dataToSend.id = hiddenIdList[index].value;
-                dataToSend.deleteButton = "Delete";
-
-                //console.log(index);
-
-                req.addEventListener('load', function(){
-                    if(req.status >= 200 && req.status < 400){
-                        var newData = JSON.parse(req.responseText);
-                        displayTable(newData);
-                        bindDeleteButtons();
-                    }
-
-                    else {
-                        console.log("Error in network request: " + req.statusText);
-                    }
-                });
-
-                req.send(JSON.stringify(dataToSend));
-                event.preventDefault();
-            });
-        }closureWrapper(index);    // wrapper function called here to create closure
-    };
 }
+	

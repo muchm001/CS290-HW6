a25c0f25330ef52fe6c6770f5d9053d69a4db108
Michael <muchm001@gmail.com>
2021-03-12 19:04:51 -0600

    ok

--- main.js
+++ main.js
@@ -30,6 +30,9 @@ var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 4000);
+
+app.use(express.static('public'));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
